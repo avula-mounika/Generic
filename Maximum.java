794322bcf632972cf2b0572a027430509ab145b8
avula-mounika <goudmounika13@gmail.com>
2021-09-03 00:21:04 +0530

    max of string

--- Maximum.java
+++ Maximum.java
@@ -1,33 +1,30 @@
 package com.bl.generic;
 
-	public class Maximum<T extends Comparable<T>> {
-		T a, b, c;
-		public Maximum(T a, T b, T c) {
-			this.a = a;
-			this.b = b;
-			this.c = c;
+public class Maximum<T extends Comparable<T>> {
+	T a, b, c;
+	public Maximum(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+		public static String testMaximum(String a, String b, String c) {
+		String max = a;
+		if(b.compareTo(max) > 0) {
+			max = b; // b is maximum
 		}
-		public T maximum() {
-			return Maximum.maximum(a, b, c);
+		if(c.compareTo(max) > 0) {
+			max = c; // c is maximum
 		}
-		//determines the largest of 3 comparable objects
-		public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
-			T max = a; //assuming a is maximum
-			if(b.compareTo(max) > 0) {
-				max = b; // b is maximum
-			}
-			if(c.compareTo(max) > 0) { //compares the c value with max 
-				max = c; // c is maximum
-			}
-			System.out.printf("Max of %s, %s and %s is %s\n",a , b , c , max);
-			return max;// returns largest object
-		}
-			public static void main(String[] args) {
-			Integer aInt = 4, bInt = 5, cInt = 6;
-			Float aFl = 7.7f, bFl = 8.8f, cFl = 9.9f;
-			new Maximum<Integer>(aInt, bInt, cInt).maximum();
-			new Maximum<Float>(aFl, bFl, cFl).maximum();
-			
-		}	
+		printMax(a, b, c, max);
+		return max;// returns largest object
+	}
+	public static <T> void printMax(T a, T b, T c, T max) {
+		System.out.printf("Max of %s, %s and %s is %s\n",a , b , c , max);
+	}
+	public static void main(String[] args) {
+		String aStr = "parrot", bStr = "pink", cStr = "white";
+	
+		Maximum.testMaximum(aStr, bStr, cStr);
+			}	
 	}
 
